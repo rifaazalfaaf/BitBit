024b251ff4044ee22161c1b5182a5c9c56e6fdb9
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-17 23:43:18 +0700

    membuat tampilan homepage

---
+++ application/controllers/homepage.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends CI_Controller {
+
+	// function __construct(){
+	// 	parent::__construct();
+	// 	$this->load->model('Materi_model');
+	// }
+
+	public function index()
+	{
+		// $data['matkul'] = $this->Materi_model->get_matkul();
+		
+		// Debug data
+		// echo '<pre>';
+		// var_dump($data['matkul']);
+		// echo '</pre>';
+
+		$this->load->view('component/header.php');
+		$this->load->view('component/navbar.php');
+		$this->load->view('main-content/home.php');
+		$this->load->view('component/footer.php');
+	}
+}
---
+++ application/views/main-content/home.php
@@ -0,0 +1,247 @@
+<div class="container pppl-home"> 
+    <div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <div class="container" style="margin-top: 20px; margin-bottom: 20px">
+                <span class="pppl-sidebar-title" style="size: 20px;">
+                   <b> Kategori Produk </b><br>
+                </span><br>
+                <span>Biji (Benih)</span><br>
+                <span>Tanaman Hias Gantung</span><br>
+                <span>Tanaman Hias Buah</span><br>
+                <span>Tanaman Hias Bunga</span><br>
+                <span>Tanaman Hias Air</span><br>
+                <span>Pupuk</span><br>
+                <span>Media Tanam</span><br>
+                <span>Pembasmi Hama</span><br>
+                <span>Perlengkapan Berkebun</span><br>
+                
+                <span class="pppl-sidebar-title" style="size: 20px">
+                <br><b>Kategori Jasa</b> <br>
+                </span><br>
+                <span>Tukang Kebun</span><br>
+                <span>Arsitektur Taman</span>
+            </div>
+        </div>
+    </div>
+    
+    <div class="col-md-9">
+        <div class="jumbotron" style="background: #71b70b">
+            <h1 style="text-align: center;">Temukan Tanaman Hias di BitBit !</h1>
+        </div>
+<!-- {{-- Bagian Produk --}} -->
+        <div>
+            <h2 class="pppl-home-title">Produk Pilihan Terbaik</h2>
+        </div>
+        <div class="card-produk">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                               <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%; margin-top:10px;margin-bottom: 20px;"><br>
+                                <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                <h5 class="harga">RP. 10.000.000</h5>
+                                <label class="alamat" for="alamat">Jakarta Utara</label>
+                                <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%;margin-top:10px;margin-bottom: 20px;"><br>
+                                <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                <h5 class="harga">RP. 10.000.000</h5>
+                                <label class="alamat" for="alamat">Jakarta Utara</label>
+                                 <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div> 
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                               <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                <h5 class="harga">RP. 10.000.000</h5>
+                                <label class="alamat" for="alamat">Jakarta Utara</label>
+                                 <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+<!-- {{-- Bagian tukang kebun --}} -->
+        <div>
+            <h2 class="pppl-home-title">Tukang Kebun Terbaik</h2>
+        </div>
+        <div class="card-produk">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png" alt="Tukang Kebun" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div> 
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png"" alt="Tukang Kebun" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </a>
+                    </div> 
+                </div>
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png"" alt="Tukang Kebun" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                 <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+<!-- {{-- bagian arsitektur --}} -->
+        <div>
+            <h2 class="pppl-home-title">Arsitektur Taman Terbaik</h2>
+        </div>
+        <div class="card-produk">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png"" alt="Arsitektur" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span>(30)
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png"" alt="Arsitektur" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                 <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div> 
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#">
+                        <div class="card">
+                            <div class="container">
+                                <img class="card-img-top" src="assets/img/tukangkebun.png"" alt="Arsitektur" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                <span class="nama-produk">Sukiman Rivero</span><br>
+                                <label class="alamat" for="alamat">Depok</label>
+                                <div class="rating">
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star checked"></span>
+                                    <span class="fa fa-star"></span>
+                                    <span class="fa fa-star"></span> (30)
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+<!-- {{-- bagian artikel --}} -->
+        <div>
+            <h2 class="pppl-home-title">Artikel Terbaik untuk Tanaman anda</h2>
+        </div>
+        <div class="card-produk">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="container">
+                            <img class="card-img-top" src="assets/img/artikel.png" alt="Artikel" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                            <label class="deskripsi" for="deskripsi">8 Cara Merawat Tanaman Efektif supaya Nggak Cepat Layu</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="container">
+                            <img class="card-img-top" src="assets/img/artikel.png" alt="Artikel" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                            <label class="deskripsi" for="deskripsi">8 Cara Merawat Tanaman Efektif supaya Nggak Cepat Layu</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div> 
+</div>
