84166f99070264f2bccaaf4772af32b117dd5c99
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-05-17 23:42:19 +0700

    Membuat header

---
+++ application/views/component/header.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<html>
+<head>
+  <title>BitBit</title>
+  <meta charset="utf-8">
+
+
+  <!-- Script -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <!-- Style -->
+  <link href="assets/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  
