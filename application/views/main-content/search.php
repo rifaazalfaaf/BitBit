6ce355d2ad8d8f735b7c0f2b7ccb491b493ea8f8
agneshata <agneshata68@gmail.com>
2020-05-18 21:27:50 +0700

    first commit

---
+++ application/views/main-content/search.php
@@ -0,0 +1,96 @@
+<div class="container pppl-home"> 
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="container" style="margin-top: 20px; margin-bottom: 20px">
+                    <span class="pppl-sidebar-title" style="size: 20px;">
+                       <b> Kategori Produk </b><br>
+                    </span><br>
+                    <span>Biji (Benih)</span><br>
+                    <span>Tanaman Hias Gantung</span><br>
+                    <span>Tanaman Hias Buah</span><br>
+                    <span>Tanaman Hias Bunga</span><br>
+                    <span>Tanaman Hias Air</span><br>
+                    <span>Pupuk</span><br>
+                    <span>Media Tanam</span><br>
+                    <span>Pembasmi Hama</span><br>
+                    <span>Perlengkapan Berkebun</span><br>
+                    
+                    <span class="pppl-sidebar-title" style="size: 20px">
+                    <br><b>Kategori Jasa</b> <br>
+                    </span><br>
+                    <span>Tukang Kebun</span><br>
+                    <span>Arsitektur Taman</span>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-9">
+    <!-- {{-- Bagian Produk --}} -->
+            <div style="margin-bottom: 20px;">
+                <span class="pppl-search-title" style=" color: #6c757d;">Hasil Pencarian . . .</span>
+            </div>
+            <div class="card-produk">
+                <div class="row">
+                    <div class="col-md-4">
+                        <a href="#">
+                            <div class="card">
+                                <div class="container">
+                                   <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%; margin-top:10px;margin-bottom: 20px;"><br>
+                                    <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                    <h5 class="harga">RP. 10.000.000</h5>
+                                    <label class="alamat" for="alamat">Jakarta Utara</label>
+                                    <div class="rating">
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star"></span>
+                                        <span class="fa fa-star"></span> (30)
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#">
+                            <div class="card">
+                                <div class="container">
+                                    <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%;margin-top:10px;margin-bottom: 20px;"><br>
+                                    <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                    <h5 class="harga">RP. 10.000.000</h5>
+                                    <label class="alamat" for="alamat">Jakarta Utara</label>
+                                     <div class="rating">
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star"></span>
+                                        <span class="fa fa-star"></span> (30)
+                                    </div>
+                                </div>
+                            </div> 
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="#">
+                            <div class="card">
+                                <div class="container">
+                                   <img class="card-img-top" src="assets/img/lidahmertua.jpg" alt="Lidah mertua" style="width:100%; margin-top:10px;margin-bottom: 20px; "><br>
+                                    <span class="nama-produk">Lidah Mertua / Sansivera lidah mertua</span>
+                                    <h5 class="harga">RP. 10.000.000</h5>
+                                    <label class="alamat" for="alamat">Jakarta Utara</label>
+                                     <div class="rating">
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star checked"></span>
+                                        <span class="fa fa-star"></span>
+                                        <span class="fa fa-star"></span> (30)
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
