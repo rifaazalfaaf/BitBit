d2549c10368799cb2bc2c1d09e69739127c75ed4
agneshata <agneshata68@gmail.com>
2020-05-31 20:08:20 +0700

    updated

--- application/views/component/navbar.php
+++ application/views/component/navbar.php
@@ -1,10 +1,10 @@
 <div class="bitbit-navbar navbar navbar-expand-sm fixed-top navbar-bitbit">
     <div class="container">
          <!-- Brand/logo -->
-        <a class="navbar-brand col-md-1" href="/"><img class="logo-bitbit mb-2" src="assets/img/logo.png" style="width: 80px;" alt=""></a>
+        <a class="navbar-brand col-md-2" href="/"><img class="logo-bitbit mb-2" src="assets/img/logo.png" style="width: 80px;" alt=""></a>
         
         
-        <div class="input-group col-md-8 mb-3 p-0" style="margin-top: 10px;">
+        <div class="input-group col-md-10 mb-3 p-0" style="margin-top: 10px;">
             <input type="text" class="form-control" id="myInput" style="color: #6b8a3c" placeholder="Cari produk atau jasa disini...">
                 <div class="input-group-append" id="myDIV">
                     <button class="btn border border-left-0" style="background: #ffffff;" type="submit">
