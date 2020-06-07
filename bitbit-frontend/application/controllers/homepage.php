416ce23f8cea0f0326dc2f044bde710d940c1963
agneshata <agneshata68@gmail.com>
2020-06-07 15:52:34 +0700

    menambahkan API tanaman

--- bitbit-frontend/application/controllers/homepage.php
+++ bitbit-frontend/application/controllers/homepage.php
@@ -3,23 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Homepage extends CI_Controller {
 
-	// function __construct(){
-	// 	parent::__construct();
-	// 	$this->load->model('Materi_model');
-	// }
+ function __construct(){
+  parent::__construct();
+     $this->API="http://localhost:9000/api";
+     $this->load->library('session');
+     $this->load->library('curl');
+     $this->load->helper('form');
+     $this->load->helper('url');
+ }
 
-	public function index()
-	{
-		// $data['matkul'] = $this->Materi_model->get_matkul();
-		
-		// Debug data
-		// echo '<pre>';
-		// var_dump($data['matkul']);
-		// echo '</pre>';
+ public function index()
+ {
+  $data['tanaman'] = json_decode($this->curl->simple_get($this->API.'/tanaman'));
+  // Debug data
+  // echo '<pre>';
+  // var_dump($data['tanaman']);
+  // echo '</pre>';
 
-		$this->load->view('component/header.php');
-		$this->load->view('component/navbar.php');
-		$this->load->view('main-content/home.php');
-		$this->load->view('component/footer.php');
-	}
+  $this->load->view('component/header.php');
+  $this->load->view('component/navbar.php');
+  $this->load->view('main-content/home.php');
+  $this->load->view('component/footer.php');
+ }
 }
