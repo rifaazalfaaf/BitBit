89a1215ccf3d389912c7d6fe9ac08807ae09bcf8
Rifaa' Zalfaa' F <rzalfaa@gmail.com>
2020-06-08 09:56:20 +0700

    Frontend : perbaikan untuk searching

--- bitbit-frontend/application/controllers/search.php
+++ bitbit-frontend/application/controllers/search.php
@@ -12,12 +12,14 @@ class Search extends CI_Controller {
  public function index()
  {
     $keyword = $this->input->get('keyword');
+    $keyword = str_replace(' ', '%20', $keyword);
     $data['tanaman'] = json_decode($this->curl->simple_get($this->API.'/tanaman?name='.$keyword));
     $data['jasa'] = json_decode($this->curl->simple_get($this->API.'/jasa?title='.$keyword));
   
   // Debug data
   // echo '<pre>';
-  // var_dump($data['jasa']);
+  // var_dump($this->API.'/tanaman?name='.$keyword);
+  // var_dump($data);
   // echo '</pre>';
 
   $this->load->view('component/header.php');
